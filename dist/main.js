47203cff27de4b110ddc2e9285ec3ca81326741c
David Mays <david.mays@outlook.com>
2023-10-17 14:44:31 -0600

    connect to hivemq

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,22 @@
-//TO build: npm run build. How do we tutn off the npm build debugger?
-// How do I add type definitions?
+// To build: npm run build. How do we tutn off the npm build debugger?
+// How to add type definitions? Here somewhere: 
+// To use client side directly, would need some kind of 
+// Notice the URL starts with ws:// for web socket, not mqtt:// or http://
+// If connecting to HiveMQ's test server, find a client ID here: https://www.hivemq.com/demos/websocket-client/
+// Can see overall here: https://www.mqtt-dashboard.com/
 console.log(mqtt);
-// const client = mqtt.connect("mqtt://test.mosquitto.org");
-const url = ""
+const url = "";
 const client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt", {
     clientId: "clientId-YJtPRW8rcR"
 });
-client.on("connect", () =>
-{
-    client.subscribe("presence", (err) =>
-    {
-        if (!err)
-        {
+client.on("connect", () => {
+    client.subscribe("presence", (err) => {
+        if (!err) {
             client.publish("presence", "Hello mqtt");
         }
     });
 });
-client.on("message", (topic, message) =>
-{
+client.on("message", (topic, message) => {
     // message is Buffer
     console.log(message.toString());
     client.end();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-//TO build: npm run build. How do we tutn off the npm build debugger?
-// How do I add type definitions?
-console.log(mqtt)
+// To build: npm run build. How do we tutn off the npm build debugger?
+// How to add type definitions? Here somewhere: 
+// To use client side directly, would need some kind of 
+// Notice the URL starts with ws:// for web socket, not mqtt:// or http://
 
+// If connecting to HiveMQ's test server, find a client ID here: https://www.hivemq.com/demos/websocket-client/
+// Can see overall here: https://www.mqtt-dashboard.com/
 
-const client = mqtt.connect("mqtt://test.mosquitto.org");
+console.log(mqtt);
 
+const url = ""
+const client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt", {
+    clientId: "clientId-YJtPRW8rcR"
+});
 client.on("connect", () =>
 {
     client.subscribe("presence", (err) =>
@@ -15,7 +22,6 @@ client.on("connect", () =>
         }
     });
 });
-
 client.on("message", (topic, message) =>
 {
     // message is Buffer
