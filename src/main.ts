8ace0fad95826885f98538fb391be5cc43eb8594
David Mays <david.mays@outlook.com>
2023-10-19 11:15:14 -0600

    ugh js imports are a mess

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,153 @@
 // How to add type definitions? Here somewhere: 
 // To use client side directly, would need some kind of 
 // Notice the URL starts with ws:// for web socket, not mqtt:// or http://
-
 // If connecting to HiveMQ's test server, find a client ID here: https://www.hivemq.com/demos/websocket-client/
 // Can see overall here: https://www.mqtt-dashboard.com/
 
-console.log(mqtt);
+// Connect packet definiton: https://github.com/mqttjs/mqtt-packet#connect
 
-const url = "ws://broker.hivemq.com:8000/mqtt"
-const client = mqtt.connect(url, {
-    clientId: "clientId-54t4sP6pzs"
-});
-client.on("connect", () =>
+///////////////////////////////////////////////////////////////
+//YES this is all spaghetti. It was made quickly for tesing..
+//////////////////////////////////////////////////////////////
+
+
+console.log(mqtt);
+let url = "ws://broker.hivemq.com:8000/mqtt";
+let client_id = "clientId-54t4sP6pzs";
+let user_name = "";
+
+const subscriptions = [];
+
+// let connect_packet = m
+
+
+
+
+const client = mqtt.connect(url);
+
+
+
+// const client = mqtt.connect(url, {
+//     clientId: client_id,
+
+// });
+
+const client_id_input = document.querySelector("#client_id_input")
+client_id_input.setAttribute("value", client_id)
+client_id_input.addEventListener("input", function (event) { client_id = event.target.value })
+
+const url_input = document.querySelector("#url_input")
+url_input.setAttribute("value", url)
+url_input.addEventListener("input", function (event) { url = event.target.value })
+
+const get_id_button = document.querySelector("#get_id")
+get_id_button.addEventListener("click", function () { window.open("https://www.hivemq.com/demos/websocket-client", "_blank") })
+
+const generate_user = () =>
 {
-    client.subscribe("presence2", (err) =>
+    var a = ["Impatient", "Blue", "Ugly", "Friendly", "Sunny", "Quick", "Intelligent", "Curious", "Elegant", "Delightful", "Playful", "Vibrant", "Clever", "Lively", "Cozy", "Radiant", "Sparkling", "Adventurous", "Mysterious", "Peaceful"]
+    var b = ["Banana", "Penguin", "Rock", "Pickle", "Noodle", "Giraffe", "Squirrel", "Sock", "Llama", "Wombat", "Pancake", "Unicorn", "Pajamas", "Bear", "Taco", "Tire", "Lobster", "Pineapple", "Kangaroo", "Beluga"];
+    var rA = Math.floor(Math.random() * a.length);
+    var rB = Math.floor(Math.random() * b.length);
+    return a[rA] + b[rB];
+}
+const user = generate_user();
+const open_conn_button = document.querySelector("#open_connection");
+const add_sub_button = document.querySelector("#add_subscription");
+const connect_form = document.querySelector("#connect_form");
+const subscribe_form = document.querySelector("#subscribe_form");
+const checkmark = document.querySelector("#checkmark")
+// const disconnect_icon = document.querySelector("#disconnect")
+
+const refresh_user = () => document.querySelector("#current_user").innerHTML = user
+const toggle_connect = () =>
+{
+    if (connect_form.hasAttribute("hidden"))
     {
-        if (!err)
+        connect_form.removeAttribute("hidden");
+        subscribe_form.setAttribute("hidden", "");
+    } else
+    {
+        connect_form.setAttribute("hidden", "")
+    }
+}
+
+const toggle_subscribe = () =>
+{
+    if (subscribe_form.hasAttribute("hidden"))
+    {
+        subscribe_form.removeAttribute("hidden");
+        connect_form.setAttribute("hidden", "");
+    } else
+    {
+        subscribe_form.setAttribute("hidden", "")
+    }
+}
+
+
+open_conn_button.addEventListener("click", toggle_connect);
+add_sub_button.addEventListener("click", toggle_subscribe);
+
+refresh_user()
+
+const subscribe = (topic) =>
+{
+    client.subscribe(topic, (err) =>
+    {
+        if (err)
         {
-            client.publish("presence2", "Hello mqtt");
+            alert('Error subscribing to the topic:', err);
+        } else
+        {
+            if (!subscriptions.includes(topic))
+            {
+                subscriptions.push(topic);
+            }
+
+            const dom_subscriptions = document.getElementById("sub_list")
+            const existing_sub = dom_subscriptions.querySelector(`li#${topic}`)
+
+            if (!existing_sub)
+            {
+                const dom_sub = document.createElement("li");
+                dom_sub.id = topic
+
+                const anchor = document.createElement("a");
+                anchor.href = "#";
+                anchor.textContent = topic
+
+                dom_sub.appendChild(anchor);
+                dom_subscriptions.appendChild(dom_sub);
+
+                // now close the subscribe box? no might want to add others.
+            }
         }
     });
+}
+
+
+client.on("connect", () =>
+{
+
+    if (checkmark.hasAttribute("hidden"))
+    {
+        checkmark.removeAttribute("hidden")
+    };
+
+    //Post username to 
+    client.publish("participants", user)
+    subscribe("participants");
+    client.publish("participants", user)
 });
 
+client.on("disconnect", () =>
+{
+
+    if (!checkmark.hasAttribute("hidden"))
+    {
+        checkmark.setAttribute("hidden", "")
+    };
+});
 
 client.on("message", (topic, message) =>
 {
@@ -30,3 +156,6 @@ client.on("message", (topic, message) =>
     console.log(message.toString());
     client.end();
 });
+
+
+
